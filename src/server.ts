0c2788895a7f69f23dc686851d67174ed4c50bba
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-05-12 21:49:48 +0530

    fixed some errors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import router from './routers/auth.route';
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/test', (req, res) => {
+    res.send('Helllo from server side');
+})
+
+app.use('/api/auth', router)
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
