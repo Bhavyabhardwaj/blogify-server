4399421a3d7ce3b34412e3b2a1fc40260f43da29
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-05-13 13:18:16 +0530

    handled error

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,22 +2,29 @@ import { Request, Response } from "express";
 import prisma from "../db/Client";
 
 const getUser = async (req: Request, res: Response) => {
-    const userId = (req as any).user;
-    if (!userId) {
-        res.status(401).json({ message: "Unauthorized" });
-    }
-    const user = await prisma.user.findUnique({
-        where: {
-            id: Number(userId),
-        },
-        select: {
-            id: true,
-            email: true,
-            username: true,
-            bio: true,
+    try {
+        const userId = (req as any).user;
+        if (!userId) {
+            res.status(401).json({ message: "Unauthorized" });
         }
-    });
-    res.status(200).json(user);
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(userId),
+            },
+            select: {
+                id: true,
+                email: true,
+                username: true,
+                bio: true,
+            }
+        });
+        res.status(200).json(user);
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "error while getting user info" })
+    }
+
 };
 
 export { getUser };
