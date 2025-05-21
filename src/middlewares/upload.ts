2833c5d6346b0befc41c7e9959a992c32aaf3410
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-05-21 13:15:34 +0530

    commented cl code for now

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,6 +1,6 @@
 import multer from "multer";
-import { storage } from "../utils/cloudinary";
+// import { storage } from "../utils/cloudinary";
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
-export default upload;
+// export default upload;
