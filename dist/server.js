9a7ded9aea82825c7104151c1ff2a1d96e85bfd9
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-08-22 14:41:32 +0530

    updated dist

--- dist/server.js
+++ dist/server.js
@@ -18,7 +18,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 // Configure CORS for production and development
 const allowedOrigins = [
-    process.env.CLIENT_URL || 'https://blogify.bhavya.live',
+    process.env.CLIENT_URL || 'https://blogifyy.bhavya.live',
     'https://blogify-c.vercel.app',
     'http://localhost:5173',
     'http://localhost:3000',
@@ -49,7 +49,7 @@ app.use('/api/likes', like_route_1.likeRouter);
 app.use('/api/bookmarks', bookmark_route_1.default);
 app.use('/api/tags', tag_route_1.default);
 app.use(errorHandler_1.default);
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 4000;
 const HOST = '0.0.0.0'; // Listen on all network interfaces
 app.listen(PORT, HOST, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
